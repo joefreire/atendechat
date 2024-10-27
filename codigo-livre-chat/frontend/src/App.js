5fd5ebcdedb04a8152aee03f3bee10b37665f6ae
dudscamily <eduardacamily121@gmail.com>
2024-10-27 13:57:04 -0300

    Atualizando projeto

---
+++ codigo-livre-chat/frontend/src/App.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { ptBR } from "@material-ui/core/locale";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { useMediaQuery } from "@material-ui/core";
+import ColorModeContext from "./layout/themeContext";
+import { SocketContext, SocketManager } from './context/Socket/SocketContext';
+
+import Routes from "./routes";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+    const [locale, setLocale] = useState();
+
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+    const preferredTheme = window.localStorage.getItem("preferredTheme");
+    const [mode, setMode] = useState(preferredTheme ? preferredTheme : prefersDarkMode ? "dark" : "light");
+
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+            },
+        }),
+        []
+    );
+
+   const theme = createTheme(
+    {
+        scrollbarStyles: {
+            "&::-webkit-scrollbar": {
+                width: '8px',
+                height: '8px',
+            },
+            "&::-webkit-scrollbar-thumb": {
+                boxShadow: 'inset 0 0 6px rgba(0, 0, 0, 0.3)',
+                backgroundColor: "#4a0176",
+            },
+        },
+        scrollbarStylesSoft: {
+            "&::-webkit-scrollbar": {
+                width: "8px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+                backgroundColor: mode === "light" ? "#F3F3F3" : "#333333",
+            },
+        },
+        palette: {
+            type: mode,
+            primary: { main: mode === "light" ? "#4a0176" : "#FFFFFF" },
+            textPrimary: mode === "light" ? "#4a0176" : "#FFFFFF",
+            borderPrimary: mode === "light" ? "#4a0176" : "#FFFFFF",
+            dark: { main: mode === "light" ? "#333333" : "#F3F3F3" },
+            light: { main: mode === "light" ? "#F3F3F3" : "#333333" },
+            tabHeaderBackground: mode === "light" ? "#EEE" : "#666",
+            optionsBackground: mode === "light" ? "#fafafa" : "#333",
+            options: mode === "light" ? "#fafafa" : "#666",
+            fontecor: mode === "light" ? "#128c7e" : "#fff",
+            fancyBackground: mode === "light" ? "#fafafa" : "#333",
+            bordabox: mode === "light" ? "#eee" : "#333",
+            newmessagebox: mode === "light" ? "#eee" : "#333",
+            inputdigita: mode === "light" ? "#fff" : "#666",
+            contactdrawer: mode === "light" ? "#fff" : "#666",
+            announcements: mode === "light" ? "#ededed" : "#333",
+            login: mode === "light" ? "#fff" : "#1C1C1C",
+            announcementspopover: mode === "light" ? "#fff" : "#666",
+            chatlist: mode === "light" ? "#eee" : "#666",
+            boxlist: mode === "light" ? "#ededed" : "#666",
+            boxchatlist: mode === "light" ? "#ededed" : "#333",
+            total: mode === "light" ? "#fff" : "#222",
+            messageIcons: mode === "light" ? "grey" : "#F3F3F3",
+            inputBackground: mode === "light" ? "#FFFFFF" : "#333",
+            barraSuperior: mode === "light" ? "linear-gradient(to right, #4a0176, #4a0176 , #4a0176)" : "#666",
+            boxticket: mode === "light" ? "#EEE" : "#666",
+            campaigntab: mode === "light" ? "#ededed" : "#666",
+            mediainput: mode === "light" ? "#ededed" : "#1c1c1c",
+
+            // Cores personalizadas adicionadas para as categorias que você forneceu:
+            conversa: { main: "#4CAF50" }, // Verde para "Em Conversa"
+            aguardando: { main: "#FFC107" }, // Amarelo para "Aguardando"
+            finalizados: { main: "#00BCD4" }, // Ciano para "Finalizados"
+            novosContatos: { main: "#2196F3" }, // Azul para "Novos Contatos"
+            tmConversa: { main: "#673AB7" }, // Roxo para "T.M. de Conversa"
+            tmEspera: { main: "#F44336" }, // Vermelho para "T.M. de Espera"
+        },
+        mode,
+    },
+    locale
+);
+
+    useEffect(() => {
+        const i18nlocale = localStorage.getItem("i18nextLng");
+        const browserLocale =
+            i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
+
+        if (browserLocale === "ptBR") {
+            setLocale(ptBR);
+        }
+    }, []);
+
+    useEffect(() => {
+        window.localStorage.setItem("preferredTheme", mode);
+    }, [mode]);
+
+
+
+    return (
+        <ColorModeContext.Provider value={{ colorMode }}>
+            <ThemeProvider theme={theme}>
+                <QueryClientProvider client={queryClient}>
+                  <SocketContext.Provider value={SocketManager}>
+                      <Routes />
+                  </SocketContext.Provider>
+                </QueryClientProvider>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+};
+
+export default App;
